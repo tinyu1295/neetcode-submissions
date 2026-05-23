8de6ec5457411a6de555c7aaf2d8e2317d0bb778
TINYU <57064177+tinyu1295@users.noreply.github.com>
2026-05-23 07:16:07 +0100

    Add: sql-alter-table - submission-3

---
+++ SQL/sql-alter-table/submission-3.sql
@@ -0,0 +1,30 @@
+CREATE TABLE books (
+  id INTEGER,
+  title TEXT,
+  author TEXT
+);
+-- Do not modify above this line --
+
+ALTER TABLE books 
+    ADD COLUMN published_year INTEGER;
+
+ALTER TABLE books
+    RENAME COLUMN id TO isbn;
+
+ALTER TABLE books
+    DROP COLUMN author;
+
+-- ALTER TABLE published_year RENAME COLUMN id TO _id INTEGER;
+
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, column_default
+FROM information_schema.columns
+WHERE table_name = 'books'
+ORDER BY column_name;
