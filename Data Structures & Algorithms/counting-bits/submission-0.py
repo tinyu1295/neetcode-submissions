f92822be71369a8dc86fb5b5c9a5d33efb3f064c
TINYU <57064177+tinyu1295@users.noreply.github.com>
2026-05-24 08:50:23 +0100

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+
+        for i in range(n+1):
+            count = 0
+            while i > 0:
+                if i & 1 == 1:
+                    count+=1
+                i = i >> 1
+            result.append(count)
+        return result
