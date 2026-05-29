405713d66a5df97c74f8a0efc787f4cf707bc5ac
TINYU <57064177+tinyu1295@users.noreply.github.com>
2026-05-29 10:34:51 +0100

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        n1, n2 = 1, 1          # fib(1), fib(2) both = 1
+        for i in range(n - 1):
+            n1, n2 = n2, n1 + n2
+        return n2
+        
+
+
