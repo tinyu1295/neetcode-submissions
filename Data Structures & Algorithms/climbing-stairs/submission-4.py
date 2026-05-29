349266e5ad89005ad78daecb149631616825dce5
TINYU <57064177+tinyu1295@users.noreply.github.com>
2026-05-29 10:33:56 +0100

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        n1 = 1
+        n2 = 2
+        for i in range(2, n):
+            n1, n2 = n2, n1 + n2
+        
+        return n2
+        
+
+
